8a556b433e1161aa3f8a693e23613048aa77533e
Chris Novick <c.r.novick@gmail.com>
2019-12-01 13:55:22 -0500

    Initial menu system

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,58 @@ mod novimem;
 
 use novimem::{proc_search::ProcSearch, NoviMem};
 use std::io::{stdin, stdout, Write};
-use std::{env, i32};
+use std::{env, i32, u8};
 
-fn interactive(mem: &mut NoviMem) {
+fn do_search(mem: &mut NoviMem, val: &[u8]) {
+    let num_results = mem.search(val);
+    println!("Found {} results", num_results);
+    if num_results <= 10 {
+        mem.print_results();
+    }
+}
+
+fn do_searchi32(mem: &mut NoviMem, cmd: &mut Vec<&str>) {
+    if let Some(search_str) = cmd.pop() {
+        if let Ok(search_int) = i32::from_str_radix(search_str, 10) {
+            do_search(mem, &search_int.to_le_bytes())
+        } else {
+            println!("Unable to parse input as u8: '{}'", search_str);
+        }
+    }
+}
+
+fn do_searchu8(mem: &mut NoviMem, cmd: &mut Vec<&str>) {
+    if let Some(search_str) = cmd.pop() {
+        if let Ok(search_int) = u8::from_str_radix(search_str, 10) {
+            do_search(mem, &search_int.to_le_bytes())
+        } else {
+            println!("Unable to parse input as i32: '{}'", search_str);
+        }
+    }
+}
+
+fn print_img(mem: &mut NoviMem) {
+
+}
+
+fn interactive(mut mem: &mut NoviMem) {
     loop {
-        print!("Search value:");
+        print!("NM>");
         stdout().flush().unwrap();
         let mut input = String::new();
         match stdin().read_line(&mut input) {
             Ok(n) => {
-                if let Ok(search_int) = i32::from_str_radix(&input[..n - 1], 10) {
-                    let num_results = mem.search(&search_int.to_le_bytes());
-                    println!("Found {} results", num_results);
-                    if num_results == 0 {
-                        break;
-                    } else if num_results <= 10 {
-                        mem.print_results();
-                    }
-                } else {
-                    println!("Unable to parse input as i32");
+                // Get the command from the input string
+                let mut parsed: Vec<&str> = input[..n - 1].split(" ").collect();
+                parsed.reverse();
+                if let Some(cmd) = parsed.pop() {
+                    match cmd {
+                        "i" => do_searchi32(&mut mem, &mut parsed),
+                        "b" => do_searchu8(&mut mem, &mut parsed),
+                        "p" => print_img(mem),
+                        "x" => break,
+                        _ => println!("Unknown command {}", cmd),
+                    };
                 }
             }
             Err(error) => println!("error: {}", error),
