a680ae919937c073a0f90e2e5e2140b9637765e9
Chris Novick <c.r.novick@gmail.com>
2019-12-02 23:32:11 -0500

    Dev snapshot

--- src/novimem/mem_image.rs
+++ src/novimem/mem_image.rs
@@ -5,18 +5,20 @@ use super::NoviMem;
 
 pub struct MemImage {
     prev_snapshot: Vec<u8>,
+    prev_addr: u64,
 }
 
 impl MemImage {
     pub fn new() -> MemImage {
         MemImage {
             prev_snapshot: Vec::<u8>::new(),
+            prev_addr: 0,
         }
     }
     pub fn print_img(&mut self, mem: &mut NoviMem, addr: u64, size: usize) {
         // Get the block of memory we care about
         if let Some(mem_block) = mem.getval(addr, size) {
-            if self.prev_snapshot.len() != size {
+            if self.prev_snapshot.len() != size || self.prev_addr != addr {
                 self.prev_snapshot.clear();
             }
             let imgx = size.sqrt();
@@ -32,14 +34,14 @@ impl MemImage {
                     } else {
                         mem_block[idx]
                     };
-                let g = if is_oversize {
-                    0
+                let g = if is_oversize { 0 } else { mem_block[idx] };
+                let b = if is_oversize {
+                    255
                 } else if is_comparable && self.prev_snapshot[idx] > mem_block[idx] {
                     255
                 } else {
                     mem_block[idx]
                 };
-                let b = if is_oversize { 255 } else { mem_block[idx] };
                 image::Rgb([r, g, b])
             });
 
@@ -48,39 +50,12 @@ impl MemImage {
             img.save("./image.bmp").unwrap();
 
             self.prev_snapshot = mem_block;
+            self.prev_addr = addr;
         } else {
             println!(
                 "Unable to get memblock in print_img() at addr {:X} of size {}",
                 addr, size
             );
         }
-
-        // if let Some(contents) = mem.get_region_contents("[heap]") {
-        //     if !self.prev_snapshot.is_empty() {
-        //         // let sz = contents.len() as u32;
-        //         let sz = 10000;
-        //         println!("Content length of {}: {} bytes", "[heap]", contents.len());
-        //         let imgx = 100u32;
-        //         let imgy = (sz / imgx) + 1;
-        //         let img = ImageBuffer::from_fn(imgx, imgy, |x, y| {
-        //             let idx = (x + (y * imgx) + offset) as usize;
-        //             if idx < contents.len() {
-        //                 let curr_byte = contents[idx];
-        //                 let prev_byte = self.prev_snapshot[idx];
-        //                 let r = contents[idx];
-        //                 let g = contents[idx];
-        //                 let b = if curr_byte != prev_byte { 255 } else { 0 };
-        //                 image::Rgb([0, 0, b])
-        //             } else {
-        //                 image::Rgb([0xFF, 0x0, 0x0])
-        //             }
-        //         });
-
-        //         let (width, height) = img.dimensions();
-        //         println!("Image of dims {} x {}", width, height);
-        //         img.save("./image.bmp").unwrap();
-        //     }
-        //     self.prev_snapshot = contents;
-        // }
     }
 }
